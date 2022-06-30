d12cbc1e915f4cf0386fa82a1f91be27e0f5e0c6
dilip-simbiotik <dilip.s@simbiotikgroup.com>
2022-07-01 00:12:11 +0530

    fix the mongo

--- Db/db.js
+++ Db/db.js
@@ -3,22 +3,22 @@ const { options } = require("../routes/routes");
 require('dotenv').config();
 
 let dbUrl = process.env.DB;
-let mongooseDb;
 
 const db = async() => {
   const options = {
     autoIndex: false, // Don't build indexes
     maxPoolSize: 10, // Maintain up to 10 socket connections
-    // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4, // Use IPv4, skip trying IPv6
   };
   try {
-    await mongoose.connect("mongodb://localhost/Profiles", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }).then(() => console.log("Connected")).catch((err) => console.log("Error check ----- ", err));
-    return mongoose;
+    await mongoose
+      .connect(dbUrl, options)
+      .then(() => {
+        console.log("DB Connected");
+      })
+      .catch((error) => console.log("Error", error));
   } catch (error) {
     throw error;
   }
